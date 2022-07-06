663887b0595c18a5ada6e427c1792da03440554d
Jakub Prymus <djhidlk@gmail.com>
2022-07-06 14:14:20 +0200

    HomePage

---
+++ android/app/src/main/kotlin/com/example/pumpsanity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pumpsanity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('images/theme_sanity.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
